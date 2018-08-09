00c9bb89e36e6a8495b07b4f03f3da45e883fe08
SP <srp19@users.noreply.github.com>
2018-08-09 04:09:28 -0500

    emr basics

---
+++ SparkScala/src/com/srp/awsemr1/basics1.scala
@@ -0,0 +1,19 @@
+package com.srp.awsemr1
+
+object basics1 {
+  def main(args: Array[String])
+  {
+    println("emr basics")
+    
+    /*
+    _______________________________________________________________________
+    #aws emr basics
+    
+    	
+   		
+   		
+    
+     */
+    
+  }
+}
